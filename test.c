7505d89d97ceff85b93dbcd3e810767d62eeb8fe
DangDat24 <dtdat24@gmail.com>
2023-10-03 21:07:19 +0700

    test

--- test.c
+++ test.c
@@ -1,11 +1,10 @@
 #include<stdio.h>
 int main() {
   int a = 81;
-  int b = 18;
   int c = 12;
   int sum;
-  sum  = a + b + c;
-  printf("Tong cua a + b + c la:%d", sum);
+  sum  = a + c;
+  printf("Tong cua a + c la:%d", sum);
 
   return 0;
 } 
